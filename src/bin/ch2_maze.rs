2155ec47540094a7666e9d0805ac477c9346c559
elderica <shigemichik@gmail.com>
2022-06-22 21:44:03 +0900

    dfs and bfs

---
+++ src/bin/ch2_maze.rs
@@ -0,0 +1,188 @@
+use classic_computer_science_problems_in_rust::generic_search::Searchable;
+use rand::prelude::*;
+use std::fmt::{Debug, Display};
+
+#[derive(Clone, Copy, PartialEq)]
+enum Cell {
+    Empty,
+    Blocked,
+    Start,
+    Goal,
+    Path,
+}
+
+impl Display for Cell {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Empty => write!(f, " "),
+            Self::Blocked => write!(f, "X"),
+            Self::Start => write!(f, "S"),
+            Self::Goal => write!(f, "G"),
+            Self::Path => write!(f, "*"),
+        }
+    }
+}
+
+struct Maze {
+    rows: isize,
+    columns: isize,
+    start: MazeLocation,
+    goal: MazeLocation,
+    grid: Vec<Vec<Cell>>,
+}
+
+impl Maze {
+    fn new() -> Self {
+        Self::generate(
+            10,
+            10,
+            MazeLocation::new(0, 0),
+            MazeLocation::new(9, 9),
+            0.2,
+        )
+    }
+
+    fn generate(
+        rows: usize,
+        columns: usize,
+        start: MazeLocation,
+        goal: MazeLocation,
+        sparseness: f64,
+    ) -> Self {
+        let mut grid = vec![vec![Cell::Empty; columns]; rows];
+        randomly_fill(&mut grid, sparseness);
+
+        grid[start.row as usize][start.column as usize] = Cell::Start;
+        grid[goal.row as usize][goal.column as usize] = Cell::Goal;
+
+        Self {
+            rows: rows as isize,
+            columns: columns as isize,
+            start,
+            goal,
+            grid,
+        }
+    }
+
+    fn mark(&mut self, path: &Vec<MazeLocation>) {
+        for ml in path {
+            self.grid[ml.row as usize][ml.column as usize] = Cell::Path;
+        }
+        self.grid[self.start.row as usize][self.start.column as usize] = Cell::Start;
+        self.grid[self.goal.row as usize][self.goal.column as usize] = Cell::Goal;
+    }
+
+    fn clear(&mut self, path: &Vec<MazeLocation>) {
+        for ml in path {
+            self.grid[ml.row as usize][ml.column as usize] = Cell::Empty;
+        }
+        self.grid[self.start.row as usize][self.start.column as usize] = Cell::Start;
+        self.grid[self.goal.row as usize][self.goal.column as usize] = Cell::Goal;
+    }
+
+    // fn euclidean_distance(&self, ml: &MazeLocation) -> f64 {
+    //     let xdist = ml.column - self.goal.column;
+    //     let ydist = ml.row - self.goal.row;
+    //     ((xdist * xdist + ydist * ydist) as f64).sqrt()
+    // }
+
+    // fn manhattan_distance(&self, ml: &MazeLocation) -> f64 {
+    //     let xdist = ml.column - self.goal.column;
+    //     let ydist = ml.row - self.goal.row;
+    //     (xdist + ydist) as f64
+    // }
+}
+
+impl Searchable<MazeLocation> for Maze {
+    fn initial(&self) -> MazeLocation {
+        MazeLocation { row: 0, column: 0 }
+    }
+
+    fn is_goal(&self, ml: &MazeLocation) -> bool {
+        self.goal == *ml
+    }
+
+    fn successors(&self, ml: &MazeLocation) -> Vec<MazeLocation> {
+        let mut locations = Vec::new();
+        let grid = &self.grid;
+        let rows = self.rows;
+        let columns = self.columns;
+
+        if ml.row + 1 < rows && grid[(ml.row + 1) as usize][ml.column as usize] != Cell::Blocked {
+            locations.push(MazeLocation::new(ml.row + 1, ml.column));
+        }
+        if ml.row > 0 && grid[(ml.row - 1) as usize][ml.column as usize] != Cell::Blocked {
+            locations.push(MazeLocation::new(ml.row - 1, ml.column));
+        }
+        if ml.column + 1 < columns
+            && grid[ml.row as usize][(ml.column + 1) as usize] != Cell::Blocked
+        {
+            locations.push(MazeLocation::new(ml.row, ml.column + 1));
+        }
+        if ml.column > 0 && grid[ml.row as usize][(ml.column - 1) as usize] != Cell::Blocked {
+            locations.push(MazeLocation::new(ml.row, ml.column - 1));
+        }
+
+        locations
+    }
+}
+
+fn randomly_fill(grid: &mut [Vec<Cell>], sparseness: f64) {
+    for row in grid.iter_mut() {
+        for cell in row.iter_mut() {
+            if random::<f64>() < sparseness {
+                *cell = Cell::Blocked;
+            }
+        }
+    }
+}
+
+impl Display for Maze {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let grid = &self.grid;
+        for row in grid {
+            let s: String = row.iter().map(Cell::to_string).collect();
+            writeln!(f, "{}", s)?;
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Debug, Hash, PartialEq, Eq, Clone)]
+struct MazeLocation {
+    row: isize,
+    column: isize,
+}
+
+impl MazeLocation {
+    fn new(row: isize, column: isize) -> Self {
+        Self { row, column }
+    }
+}
+
+fn main() {
+    println!("----------------------------------------------------");
+    let mut m1 = Maze::new();
+    println!("{}", m1);
+    println!("====================================================");
+    if let Some(solution1) = m1.dfs() {
+        m1.mark(&solution1);
+        println!("{}", m1);
+        m1.clear(&solution1);
+    } else {
+        println!("No solution found using depth-first search!")
+    }
+    println!("----------------------------------------------------");
+    let mut m2 = Maze::new();
+    println!("{}", m2);
+    println!("====================================================");
+    if let Some(solution1) = m2.bfs() {
+        m2.mark(&solution1);
+        println!("{}", m2);
+        m2.clear(&solution1);
+    } else {
+        println!("No solution found using depth-first search!")
+    }
+    println!("----------------------------------------------------");
+}
---
+++ src/generic_search.rs
@@ -0,0 +1,155 @@
+use std::{
+    cmp::Ordering,
+    collections::{HashSet, VecDeque},
+    fmt::Debug,
+    hash::Hash,
+};
+
+pub fn linear_contains<T>(v: &[T], key: &T) -> bool
+where
+    T: Ord,
+{
+    v.iter().any(|e| e.cmp(key) == Ordering::Equal)
+}
+
+pub fn binary_contains<T>(v: &[T], key: &T) -> bool
+where
+    T: Ord + std::clone::Clone,
+{
+    let mut s = v.to_vec();
+    s.sort_unstable();
+    s.binary_search(key).is_ok()
+}
+
+pub trait Searchable<State>
+where
+    State: Clone + Hash + Eq + Debug,
+{
+    fn initial(&self) -> State;
+    fn is_goal(&self, s: &State) -> bool;
+    fn successors(&self, s: &State) -> Vec<State>;
+
+    fn dfs(&self) -> Option<Vec<State>> {
+        let mut visited: HashSet<State> = HashSet::new();
+        let mut history: Vec<State> = Vec::new();
+        let mut stack: VecDeque<State> = VecDeque::new();
+        stack.push_back(self.initial());
+
+        while let Some(current_state) = stack.pop_back() {
+            history.push(current_state.clone());
+
+            if self.is_goal(&current_state) {
+                return Some(history);
+            }
+
+            for successor in self.successors(&current_state).into_iter() {
+                if visited.insert(successor.clone()) {
+                    stack.push_back(successor);
+                }
+            }
+        }
+
+        None
+    }
+
+    fn bfs(&self) -> Option<Vec<State>> {
+        let mut visited: HashSet<State> = HashSet::new();
+        let mut history: Vec<State> = Vec::new();
+        let mut queue: VecDeque<State> = VecDeque::new();
+        queue.push_back(self.initial());
+
+        while let Some(current_state) = queue.pop_front() {
+            history.push(current_state.clone());
+
+            if self.is_goal(&current_state) {
+                return Some(history);
+            }
+
+            for successor in self.successors(&current_state).into_iter() {
+                if visited.insert(successor.clone()) {
+                    queue.push_back(successor);
+                }
+            }
+        }
+
+        None
+    }
+}
+
+// pub struct Node<T>
+// where
+//     T: Clone + Hash + Eq,
+// {
+//     state: T,
+//     cost: f64,
+//     heuristic: f64,
+// }
+
+// impl<T> Node<T>
+// where
+//     T: Clone + Hash + Eq,
+// {
+//     fn new(state: T) -> Self {
+//         Self {
+//             state,
+//             cost: 0.0,
+//             heuristic: 0.0,
+//         }
+//     }
+
+//     fn new_with_cost(state: T, cost: f64, heuristic: f64) -> Self {
+//         Self {
+//             state,
+//             cost,
+//             heuristic,
+//         }
+//     }
+// }
+
+// impl<T> PartialEq for Node<T>
+// where
+//     T: Clone + Hash + Eq,
+// {
+//     fn eq(&self, other: &Self) -> bool {
+//         let mine = self.cost + self.heuristic;
+//         let theirs = other.cost + other.heuristic;
+//         mine.eq(&theirs)
+//     }
+// }
+
+// impl<T> PartialOrd for Node<T>
+// where
+//     T: Clone + Hash + Eq,
+// {
+//     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+//         let mine = self.cost + self.heuristic;
+//         let theirs = other.cost + other.heuristic;
+//         mine.partial_cmp(&theirs)
+//     }
+// }
+
+// pub fn dfs<T>(initial: T, goal_test: fn(&T) -> bool, successors: fn(&T) -> Vec<T>) -> Option<Vec<T>>
+// where
+//     T: Clone + Hash + Eq,
+// {
+//     let mut visited: HashSet<T> = HashSet::new();
+//     let mut history: Vec<T> = Vec::new();
+//     let mut stack: VecDeque<T> = VecDeque::new();
+//     stack.push_back(initial);
+
+//     while let Some(current_state) = stack.pop_back() {
+//         history.push(current_state.clone());
+
+//         if goal_test(&current_state) {
+//             return Some(history);
+//         }
+
+//         for succ in successors(&current_state).into_iter() {
+//             if visited.insert(succ.clone()) {
+//                 stack.push_back(succ);
+//             }
+//         }
+//     }
+
+//     None
+// }
